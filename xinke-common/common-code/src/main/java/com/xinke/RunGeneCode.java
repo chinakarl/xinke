9464bf53a8e39b192b1a877f17616d1b8c1bec41
zhaihaixiang <netzhaihaixiang@sina.com>
2019-09-25 10:42:38 +0800

    add

---
+++ xinke-common/common-code/src/main/java/com/xinke/RunGeneCode.java
@@ -0,0 +1,13 @@
+package com.xinke;
+
+/**
+ * Hello world!
+ *
+ */
+public class RunGeneCode
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
